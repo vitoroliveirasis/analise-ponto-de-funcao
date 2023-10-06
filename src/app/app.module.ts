214588b91b46692a154103d9ed224767509d8c19
Vitor Oliveira <vitor.oliveira@sisconsultoria.com.br>
2023-10-06 11:18:13 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabelaComponent } from './tabela/tabela.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// import { tabela } from '../src/app/tabela'
+// import { header } from '../src/app/tabela'
+// import { button } from '../src/app/tabela'
 
 if (environment.production) {
   enableProdMode();
